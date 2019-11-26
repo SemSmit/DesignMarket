49d2a3ad432f9147cbf880afae752abd95bb09f0
Ubuntu <ubuntu@ip-172-31-33-93.eu-central-1.compute.internal>
2019-11-26 16:05:59 +0000

    updated INSTALLED APPS in settings.py

--- market/market/settings.py
+++ market/market/settings.py
@@ -41,12 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
-    'designs',
     'request',
     'jquery',
     'accounts',
     'cart',
     'home',
+    'designs',
 ]
 
 MIDDLEWARE = [
